8b001adb9f2b2e61de3dacb065805eeed9578e0e
Yixin <yxyxchen@bu.edu>
2019-01-15 10:49:17 -0500

    daily

--- stan/fixInputStan.R
+++ stan/fixInputStan.R
@@ -6,8 +6,34 @@ rstan_options(auto_write = TRUE) # default settings borrowed somewhere
 options(mc.cores = parallel::detectCores())# enable multi-core precessors 
 library('tidyr')
 source('stan/singleFittingStan.R')
-
 model = stan_model(file = "stan/model.stan")
-for(i in 101: 125){
-  singleFittingStan(2, i)
+
+# fitting the model
+source('subFxs/wtwSettings.R') 
+source('subFxs/fittingFxs.R')
+load('outputs/fixInputSimData/initialSpace.RData') # need nComb(for loop), wIni, nPara(for initialize solutions)
+
+for(condIdx in 1:2){
+  # choose the condition
+  cond = conditions[condIdx]
+  wIni = wInis[[cond]]
+  tMax = ifelse(cond == 'HP', tMaxs[1], tMaxs[2])
+  if(condIdx == 1){
+    load('outputs/fixInputSimData/rawHPData.RData')
+    timeWaitedList = rawHPData$timeWaited[,1,]
+    trialEarningsList = rawHPData$trialEarnings[,1,]
+    rm(rawHPData)
+  }else{
+    load('outputs/fixInputSimData/rawLPData.RData')
+    timeWaitedList = rawLPData$timeWaited[,1,]
+    trialEarningsList = rawLPData$trialEarnings[,1,]
+    rm(rawLPData)
+  }
+  for(combIdx in 1: nPara){
+    # choose the combIdx
+    timeWaited = timeWaitedList[combIdx,];
+    trialEarnings = trialEarningsList[combIdx,];
+    fileName = sprintf("stanOutPuts/fixInputSimData/%s_$d.txt", cond, combIdx)
+    singleFittingStan(combIdx, cond, wIni, timeWaited, trialEarnings, )
+  }
 }
